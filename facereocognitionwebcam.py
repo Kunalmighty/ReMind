fdf875bb64a39c7c6a5aca5d5a4d7b21527485a5
Leila Kassiri <leila.kassiri@gmail.com>
2018-03-02 17:52:43 -0500

    can identify faces in images and on webcam

---
+++ facereocognitionwebcam.py
@@ -0,0 +1,183 @@
+#code adapted from https://realpython.com/blog/python/face-recognition-with-python/#cascades-in-practice and https://github.com/informramiz/opencv-face-recognition-python
+
+
+import cv2
+import os
+import numpy as np
+
+#matplotlib for display our images
+import matplotlib.pyplot as plt
+
+subjects = ["", "Leila Kassiri", "Kunal"]
+
+#function to detect face using OpenCV
+def detect_face(img):
+    #convert the test image to gray image as opencv face detector expects gray images
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    #load OpenCV face detector, I am using LBP which is fast
+    #there is also a more accurate but slow Haar classifier
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    #let's detect multiscale (some images may be closer to camera than others) images
+    #result is a list of faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
+    
+    #if no faces are detected then return original img
+    if (len(faces) == 0):
+        return None, None
+    
+    #under the assumption that there will be only one face,
+    #extract the face area
+    (x, y, w, h) = faces[0]
+    
+    #return only the face part of the image
+    return gray[y:y+w, x:x+h], faces[0]
+
+#this function will read all persons' training images, detect face from each image
+#and will return two lists of exactly same size, one list 
+# of faces and another list of labels for each face
+def prepare_training_data(data_folder_path):
+    
+    #------STEP-1--------
+    #get the directories (one directory for each subject) in data folder
+    dirs = os.listdir(data_folder_path)
+    
+    #list to hold all subject faces
+    faces = []
+    #list to hold labels for all subjects
+    labels = []
+    
+    #let's go through each directory and read images within it
+    for dir_name in dirs:
+        
+        #our subject directories start with letter 's' so
+        #ignore any non-relevant directories if any
+        if not dir_name.startswith("s"):
+            continue;
+            
+        #------STEP-2--------
+        #extract label number of subject from dir_name
+        #format of dir name = slabel
+        #, so removing letter 's' from dir_name will give us label
+        label = int(dir_name.replace("s", ""))
+        
+        #build path of directory containin images for current subject subject
+        #sample subject_dir_path = "training-data/s1"
+        subject_dir_path = data_folder_path + "/" + dir_name
+        
+        #get the images names that are inside the given subject directory
+        subject_images_names = os.listdir(subject_dir_path)
+        
+        #------STEP-3--------
+        #go through each image name, read image, 
+        #detect face and add face to list of faces
+        for image_name in subject_images_names:
+            
+            #ignore system files like .DS_Store
+            if image_name.startswith("."):
+                continue;
+            
+            #build image path
+            #sample image path = training-data/s1/1.pgm
+            image_path = subject_dir_path + "/" + image_name
+
+            #read image
+            image = cv2.imread(image_path)
+            
+            #display an image window to show the image 
+            cv2.imshow("Training on image...", image)
+            cv2.waitKey(100)
+            
+            #detect face
+            face, rect = detect_face(image)
+            
+            #------STEP-4--------
+            #for the purpose of this tutorial
+            #we will ignore faces that are not detected
+            if face is not None:
+                #add face to list of faces
+                faces.append(face)
+                #add label for this face
+                labels.append(label)
+            
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+    
+    return faces, labels
+
+faces, labels = prepare_training_data("training-data")
+print("Total faces: ", len(faces))
+print("Total labels: ", len(labels))
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+#train our face recognizer of our training faces
+face_recognizer.train(faces, np.array(labels))
+
+#function to draw rectangle on image 
+#according to given (x, y) coordinates and 
+#given width and heigh
+def draw_rectangle(img, rect):
+    (x, y, w, h) = rect
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+#function to draw text on give image starting from
+#passed (x, y) coordinates. 
+def draw_text(img, text, x, y):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+
+   #this function recognizes the person in image passed
+#and draws a rectangle around detected face with name of the 
+#subject
+def predict(frame, rect):
+    (x, y, w, h) = rect
+    #make a copy of the image as we don't want to chang original image
+    img = frame[y:y+h, x:x+w]
+    #detect face from the image
+    face, rect = detect_face(img)
+
+    if not face is None:
+        #predict the image using our face recognizer 
+        label= face_recognizer.predict(face)
+        print(label)
+        
+        if label[1] > 30:
+            return label
+    return -1
+
+video_capture = cv2.VideoCapture(0)
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for rect in faces:
+        draw_rectangle(frame, rect)
+
+        label = predict(frame, rect)
+        if label != -1:
+            #get name of respective label returned by face recognizer
+            label_text = subjects[label[0]]
+            draw_text(frame, label_text, rect[0], rect[1]-5)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
